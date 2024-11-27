09b3d5a235e97b1e3d220255c8cfc10ae03a3b47
Ananmay 125 <ananmay125@gmail.com>
2024-11-27 20:49:29 +0530

    main page css #2

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,22 @@ export default function Home() {
           <img src='favicon.ico' className='avatar'></img>
         </ul>
       </nav>
-      <main></main>
+      <main>
+        <nav className='side-bar'>
+          <ul className='side-bar-list'>
+              <li className='side-bar-li'>Comments</li>
+              <li className='side-bar-li'>Posts</li>
+              <li className='side-bar-li'>Notifications</li>
+              <li className='side-bar-li'>About Sphere</li>
+          </ul>
+        </nav>
+        <div className='scroll-div'>
+          <div className='post'>
+            <textarea className='post-input' placeholder="What's on your mind bro?"></textarea>
+          </div>
+          <div className='content-div'></div>
+        </div>
+      </main>
     </>
   );
 }
