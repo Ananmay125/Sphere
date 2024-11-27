cefe5a40aec4519624de876f35cfacc6e1f7a57c
Ananmay 125 <ananmay125@gmail.com>
2024-11-27 20:10:07 +0530

    main page css

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,19 @@ import './page.css';
 
 export default function Home() {
   return (
-    <h1>Sphere</h1>
+    <>
+      <nav>
+        <img src='image.png' className='logo'></img>
+        <h1 className="main-heading">Sphere</h1>
+        <ul>
+          <li className='nav-li'>Home</li>
+          <li className='nav-li'>Chat</li>
+          <li className='nav-li'>Friends</li>
+          <li className='nav-li'>Spicy_32</li>
+          <img src='favicon.ico' className='avatar'></img>
+        </ul>
+      </nav>
+      <main></main>
+    </>
   );
 }
